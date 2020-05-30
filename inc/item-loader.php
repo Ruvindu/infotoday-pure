f05203e729eabd104e73b921d6dc83a5067925f5
Ruvindu <53707613+Ruvindu@users.noreply.github.com>
2020-05-30 10:47:32 +0530

    item laoder

--- inc/item-loader.php
+++ inc/item-loader.php
@@ -2,9 +2,55 @@
 
   if (isset($_GET['item'])) {
   	
-  	
+  	$get_selected_itemsQ = "SELECT * FROM `newspaper` WHERE `name` = '{$_GET['item']}'";
+
+  	$get_selected_itemsQ = mysqli_query($con, $get_selected_itemsQ);
+
+  	if ($get_selected_itemsQ) {
+	  		
+  		while ($item = mysqli_fetch_assoc($get_selected_itemsQ)) {
+  			
+  			$thumbnail = base64_encode($item['thumbnail']);
+
+  			echo "<div class=\"card\" style=\"width: 18rem;\">
+                <img class=\"card-img-top\" src=\"data:image/jpeg;base64,{$thumbnail}\" alt=\"Thumbnail\" >
+                    <div class=\"card-body\">
+                    <h5 class=\"card-title\">{$item['name']}</h5>
+                    <p class=\"card-text\">{$item['description']},<b> {$item['date']}</b></p>
+                    <a href=\"index.php?id-subscribe={$item['newspaper_id']}\" class=\"btn btn-primary\"> Subscribe </a>
+                    <a href=\"index.php?id-cart={$item['newspaper_id']}\" class=\"btn btn-danger\">Add to cart</a>
+                </div>
+            </div>";
+           
+
+  		}
+  	}
 
   }else{
+
+  	$get_selected_itemsQ = "SELECT * FROM `newspaper`";
+
+  	$get_selected_itemsQ = mysqli_query($con, $get_selected_itemsQ);
+
+  	if ($get_selected_itemsQ) {
+	  		
+  		while ($item = mysqli_fetch_assoc($get_selected_itemsQ)) {
+  			
+  			$thumbnail = base64_encode($item['thumbnail']);
+
+  			echo "<div class=\"card\" style=\"width: 18rem;\">
+                <img class=\"card-img-top\" src=\"data:image/jpeg;base64,{$thumbnail}\" alt=\"Thumbnail\" >
+                    <div class=\"card-body\">
+                    <h5 class=\"card-title\">{$item['name']}</h5>
+                    <p class=\"card-text\">{$item['description']},<b> {$item['date']}</b></p>
+                    <a href=\"index.php?id-subscribe={$item['newspaper_id']}\" class=\"btn btn-primary\"> Subscribe </a>
+                    <a href=\"index.php?id-cart={$item['newspaper_id']}\" class=\"btn btn-danger\">Add to cart</a>
+                </div>
+            </div>";
+           
+
+  		}
+  	}
 
 
   }
--- index.old.php
+++ index.old.php
@@ -10,20 +10,20 @@
 
 <html>
 <head>
-  <title>InfoToday</title>
+	<title>InfoToday</title>
 
-  <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
       
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 
-    <link rel="stylesheet" type="text/css" href="css/custom.css">
+  	<link rel="stylesheet" type="text/css" href="css/custom.css">
 </head>
 
 <body class="background">
@@ -44,15 +44,15 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="cart.php">Cart</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="aboutus.php">About us</a>
-      </li>
+			<li class="nav-item active">
+				<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="cart.php">Cart</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="aboutus.php">About us</a>
+			</li>
 
       <?php
 
@@ -75,7 +75,7 @@
       ?>
 
 
-      </ul>
+			</ul>
             
 
                     <?php
@@ -109,64 +109,97 @@
         </nav>
 
 
-
-
         <div class="container-fluid">
             
             <div class="row mt-4">
                 <div class="col-md-3">
                     <h3>Categories</h3>
-
-                    <div id="accordion" class="mt-4 mb-4">
-                     
-
-                      <?php 
-
-                          $get_catsQ = "SELECT * FROM `category`";
-
-                          $res_cats = mysqli_query($con, $get_catsQ);
-
-                          if ($res_cats) {
-                                    
-                          while($cat = mysqli_fetch_assoc($res_cats)){
-
-                              /* get all newspapers category wise */
-
-                              $cat_itemsQ = "SELECT `name` FROM `newspaper` WHERE `category_id` = {$cat['category_id']} GROUP BY `name`";
-
-                              $res_cat_items = mysqli_query($con, $cat_itemsQ);
-
-                              echo "<div class=\"card\">
-                                    <div class=\"card-header\" id=\"headingOne\">
-                                    <h5 class=\"mb-0\">
-                                    <button class=\"btn btn-link custom_link\" data-toggle=\"collapse\" data-target=\"#collapse{$cat['category_id']} \" aria-expanded=\"false\" aria-controls=\"collapse{$cat['category_id']}\">
-
-                                            {$cat['category_name']}
-                                    </button>
-                                    </h5>
-                                    </div>
-
-
-                                    <div id=\"collapse{$cat['category_id']}\" class=\"collapse\" aria-labelledby=\"headingOne\" data-parent=\"#accordion\">
-                                    <div class=\"card-body\">
-                                          <dt>";
-
-                                            while ($item = mysqli_fetch_assoc($res_cat_items)) {
-                                              echo "<dd><a href='index.blade.php?item={$item['name']}' >{$item['name']} </a></dd>";
-                                            }
-                              
-                              echo        "</dt>
-                                    </div>
-                                    </div>
-                                    </div>
-                              ";
-                          }
-                         }
-
-                   ?>
                     
-                  </div>
+                    <div id="accordion" class="mt-4 mb-4">
+                      <div class="card">
+                        <div class="card-header" id="headingOne">
+                          <h5 class="mb-0">
+                            <button class="btn btn-link custom_link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                              Pets and Animals
+                            </button>
+                          </h5>
+                        </div>
 
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                          <div class="card-body">
+                            <?php
+                              $catname="pets_and_animal";
+                              $Pets_cartegory="SELECT newspaper.name FROM newspaper,category where category.category_id=newspaper.category_id and category.category_name='".$catname."'";
+                              $PetsResult=mysqli_query($con,$Pets_cartegory);
+                              while($Petsrecord=mysqli_fetch_assoc($PetsResult))
+                                 {
+                            ?>
+                            <button class="btn btn-link custom_link"  data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                              <?php echo $Petsrecord['name'];   ?>
+                            </button><br>
+                            
+                            <?php
+                              }
+                            ?>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="card">
+                        <div class="card-header" id="headingTwo">
+                          <h5 class="mb-0">
+                            <button class="btn btn-link custom_link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                              Sport and Health
+                            </button><br>
+                            
+                          </h5>
+                        </div>
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                          <div class="card-body">
+                            <?php
+                              $catname="sports_and_heal";
+                              $Sport_cartegory="SELECT newspaper.name FROM newspaper,category where category.category_id=newspaper.category_id and category.category_name='".$catname."'";
+                              $SportResult=mysqli_query($con,$Sport_cartegory);
+                              while($Sportrecord=mysqli_fetch_assoc($SportResult))
+                                 {
+                            ?>
+                            <button class="btn btn-link custom_link"  data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                              <?php echo $Sportrecord['name'];   ?>
+                            </button><br>
+                            <?php
+                              }
+                            ?>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="card">
+                        <div class="card-header" id="headingThree">
+                          <h5 class="mb-0">
+                            <button class="btn btn-link custom_link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                              Technology
+                            </button>
+                          </h5>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                          <div class="card-body">
+                            <?php
+                              $catname="technology";
+                              $Tec_cartegory="SELECT newspaper.name FROM newspaper,category where category.category_id=newspaper.category_id and category.category_name='".$catname."'";
+                              $TechResult=mysqli_query($con,$Tec_cartegory);
+                              while($Techrecord=mysqli_fetch_assoc($TechResult))
+                                 {
+                            ?>
+                            <button class="btn btn-link custom_link"  data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <?php echo $Techrecord['name'];   ?>
+                            </button><br>
+                            <?php
+                              }
+                            ?>
+                            
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    
                 </div>
 
                 <div class="col-md-9">
@@ -214,8 +247,14 @@
                       </div>
                     </div>
 
-                    <!--          Items display            -->
+<!------------------------------------------------- this one ------------------------------------------------------------->
                     <div class="card-deck mt-3">
+                      <?php if(isset($_POST['AddToPurches']))
+                        {
+                          $Categories="SELECT * FROM newspaper,category where newspaper.category_id=category.category_id and category.category_name='".$catname."'";
+                          $result=mysqli_query($conn,$SQuery);
+                        }
+                        ?>
                           <div class="card" style="width: 18rem;">
                             <img class="card-img-top" src="imgs/products/1.jpg" alt="Card image cap">
                             <div class="card-body">
@@ -225,8 +264,9 @@
                               <a href="#" class="btn btn-danger">Add to cart</a>
                             </div>
                           </div>
+                      <?php  ?>    
 
-                          <div class="card" style="width: 18rem;">
+                          <!--div class="card" style="width: 18rem;">
                             <img class="card-img-top" src="imgs/products/2.jpg" alt="Card image cap">
                             <div class="card-body">
                               <h5 class="card-title">Pariganaka</h5>
@@ -240,7 +280,7 @@
                             <img class="card-img-top" src="imgs/products/3.jpg" alt="Card image cap">
                             <div class="card-body">
                               <h5 class="card-title">Pariganaka</h5>
-                              <p class="card-text">Wijaya pariganaka monthly magazine,<b> January 20218</b></p>
+                              <p class="card-text">Wijaya pariganaka monthly magazine,<b> January 2019</b></p>
                               <a href="#" class="btn btn-primary">Preview</a>
                               <a href="#" class="btn btn-danger">Add to cart</a>
                             </div>
@@ -250,11 +290,11 @@
                             <img class="card-img-top" src="imgs/products/3.jpg" alt="Card image cap">
                             <div class="card-body">
                               <h5 class="card-title">Pariganaka</h5>
-                              <p class="card-text">Wijaya pariganaka monthly magazine,<b> January 20218</b></p>
+                              <p class="card-text">Wijaya pariganaka monthly magazine,<b> January 2020</b></p>
                               <a href="#" class="btn btn-primary">Preview</a>
                               <a href="#" class="btn btn-danger">Add to cart</a>
                             </div>
-                          </div>
+                          </div-->
                           
                     </div>
 
